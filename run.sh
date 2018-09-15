758392436056b6bb588e854b71c5f3240c911247
algorythm <anders@wiberg.tech>
2018-09-15 20:48:30 +0200

    Automatically install dependencies

--- run.sh
+++ run.sh
@@ -2,21 +2,61 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+function questionY() {
+	echo -e -n "[$COLOR_YELLOW""?""$COLOR_RESET] " $1 "[Y/n]? "
+	read response
+	case "$response" in
+		[nN][oO]|[nN])
+			false
+			;;
+		*)
+			true
+			;;
+	esac
+}
+
+function install() {
+  if questionY "Do you want to install it"
+  then
+    if ! [ -x "$(apt-get)" ]; then
+      echo "Error: apt-get does not exist."
+      exit 1
+    else
+      sudo apt-get install $1
+    fi
+  else
+    exit 1
+  fi
+}
+function pipinstall() {
+  if questionY "Do you want to install it"
+  then
+    if ! [ -x "$(pip3)" ]; then
+      echo "Error: pip3 does not exist."
+      exit 1
+    else
+      sudo pip3 install $1
+    fi
+  else
+    exit 1
+  fi
+}
+
 if ! [ -x "$(command -v python3)" ]; then
   echo 'Error: python3 is not installed.' >&2
-  exit 1
+  install python3
 fi
 
 if ! [ -x "$(command -v pip3)" ]; then
   echo 'Error: pip3 is not installed.' >&2
   echo 'Run: sudo apt-get install -y python3-pip' >&2
-  exit 1
+  install python3-pip
 fi
 
 if ! [ -x "$(command -v virtualenv)" ]; then
   echo 'Error: virtualenv is not installed.' >&2
   echo 'Run: sudo pip3 install virtualenv' >&2
-  exit 1
+  pipinstall virtualenv
 fi
 
 
